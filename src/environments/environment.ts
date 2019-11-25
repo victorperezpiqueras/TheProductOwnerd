d522370b33ca7fd142b0d219349bbc869b7efaf4
victorperez98 <victorperezpiqueras@gmail.com>
2019-11-25 14:10:56 +0100

    Probando version del env #13

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,12 +8,12 @@
 // This is useful for granularity you might need beyond just the environment.
 // Note that as usual, any environment variables you expose through it will end up in your
 // bundle, and you should not use it for any sensitive information like passwords or keys.
-//import { env } from 'src/environments/.env';
+import { env } from 'src/environments/.env';
 
 export const environment = {
   production: false,
   hmr: true,
-  //version: env.npm_package_version + '-dev',
+  version: '1.0.0' + '-dev',
   serverUrl: '/api',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'fr-FR']
